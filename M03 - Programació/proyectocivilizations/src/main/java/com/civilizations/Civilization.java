b1b26ebc3639a708dc887fed27a97e7baf73a4bc
Barg0s <dav.bargados@gmail.com>
2024-05-04 17:42:32 +0200

    Civilizacion+Cuartel
    Creación de la clase Civilizacion + la interface MIlitaryUnit(ambos en proceso)

---
+++ M03 - Programació/proyectocivilizations/src/main/java/com/civilizations/Civilization.java
@@ -0,0 +1,19 @@
+package com.civilizations;
+
+import java.util.ArrayList;
+
+public class Civilization {
+    int technologyDefense ;
+    int technologyAtack ;
+    int wood;
+    int iron;
+    int food;
+    int mana;
+    int magicTower;
+    int church;
+    int farm;
+    int smithy;
+    int carpentry;
+    int battles;
+    /*ArrayList<MilitaryUnit> army = new ArrayList[9];*/
+}
