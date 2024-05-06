dc32b3857a54ea6a49f90ef8da318645f1c9b6db
UnaiMunoz <unaimunoz2024@gmail.com>
2024-05-06 21:07:29 +0200

    Canvios clases Attack Unit
    Constructor, Getter y Setters

--- M03 - Programació/proyectocivilizations/src/main/java/com/civilizations/AttackUnit.java
+++ M03 - Programació/proyectocivilizations/src/main/java/com/civilizations/AttackUnit.java
@@ -1,5 +1,62 @@
 package com.civilizations;
 
 public class AttackUnit {
+
+    int armor;
+    int initialArmor;
+    int baseDamage;
+    int experience;
+    boolean sanctified;
+
+    public AttackUnit(int armor, int initialArmor, int baseDamage, int experience, boolean sanctified) {
+
+        this.armor = armor;
+        this.initialArmor = initialArmor;
+        this.baseDamage = baseDamage;
+        this.experience = experience;
+        this.sanctified = sanctified;
+
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public int getBaseDamage() {
+        return baseDamage;
+    }
+
+
+    public int getInitialArmor() {
+        return initialArmor;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public boolean getSanctified() {
+        return sanctified;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void setInitialArmor(int initialArmor) {
+        this.initialArmor = initialArmor;
+    }
+
+    public void setBaseDamage(int baseDamage) {
+        this.baseDamage = baseDamage;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public void setSanctified(boolean sanctified) {
+        this.sanctified = sanctified;
+    }
     
 }
