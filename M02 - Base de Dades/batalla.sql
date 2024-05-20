ef7a30988b7e4a47fa2cfccbd3a7ea87d2f90458
Barg0s <dav.bargados@gmail.com>
2024-05-20 18:00:06 +0200

    BDD cambios
    a plsql

--- M02 - Base de Dades/batalla.sql
+++ M02 - Base de Dades/batalla.sql
@@ -1,65 +1,58 @@
-use civilizations;
-/*DROP TABLE Civilization_attack_stats;
-DROP TABLE Civilization_defense_stats;
-DROP TABLE Civilization_special_stats;
-DROP TABLE Battle_stats;
-DROP TABLE Battle_log;
-DROP TABLE Enemy_attack_stats;*/
 
 CREATE TABLE Civilization_attack_stats (
-    civilization_id INT NOT NULL,
-    num_battle INT NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    initial INT,
-    drops INT,
+    civilization_id NUMBER(10) NOT NULL,
+    num_battle NUMBER(10) NOT NULL,
+    type VARCHAR2(50) NOT NULL,
+    initial NUMBER(10),
+    drops NUMBER(10),
     PRIMARY KEY (civilization_id, num_battle, type),
     FOREIGN KEY (civilization_id) REFERENCES civilization_stats(civilization_id)
 );
 
 CREATE TABLE Civilization_defense_stats (
-    civilization_id INT NOT NULL,
-    num_battle INT NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    initial INT,
-    drops INT,
+    civilization_id NUMBER(10) NOT NULL,
+    num_battle NUMBER(10) NOT NULL,
+    type VARCHAR2(50) NOT NULL,
+    initial NUMBER(10),
+    drops NUMBER(10),
     PRIMARY KEY (civilization_id, num_battle, type),
     FOREIGN KEY (civilization_id) REFERENCES civilization_stats(civilization_id)
 );
 
 CREATE TABLE Civilization_special_stats (
-    civilization_id INT NOT NULL,
-    num_battle INT NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    initial INT,
-    drops INT,
+    civilization_id NUMBER(10) NOT NULL,
+    num_battle NUMBER(10) NOT NULL,
+    type VARCHAR2(50) NOT NULL,
+    initial NUMBER(10),
+    drops NUMBER(10),
     PRIMARY KEY (civilization_id, num_battle, type),
     FOREIGN KEY (civilization_id) REFERENCES civilization_stats(civilization_id)
 );
 
 CREATE TABLE Battle_stats (
-    civilization_id INT NOT NULL,
-    num_battle INT NOT NULL,
-    wood_acquired INT,
-    iron_acquired INT,
+    civilization_id NUMBER(10) NOT NULL,
+    num_battle NUMBER(10) NOT NULL,
+    wood_acquired NUMBER(10),
+    iron_acquired NUMBER(10),
     PRIMARY KEY (civilization_id, num_battle),
     FOREIGN KEY (civilization_id) REFERENCES civilization_stats(civilization_id)
 );
 
 CREATE TABLE Battle_log (
-    civilization_id INT NOT NULL,
-    num_battle INT NOT NULL,
-    num_line INT NOT NULL,
-    log_entry TEXT,
+    civilization_id NUMBER(10) NOT NULL,
+    num_battle NUMBER(10) NOT NULL,
+    num_line NUMBER(10) NOT NULL,
+    log_entry CLOB,
     PRIMARY KEY (civilization_id, num_battle, num_line),
     FOREIGN KEY (civilization_id, num_battle) REFERENCES Battle_stats(civilization_id, num_battle)
 );
 
 CREATE TABLE Enemy_attack_stats (
-    civilization_id INT NOT NULL,
-    num_battle INT NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    initial INT,
-    drops INT,
+    civilization_id NUMBER(10) NOT NULL,
+    num_battle NUMBER(10) NOT NULL,
+    type VARCHAR2(50) NOT NULL,
+    initial NUMBER(10),
+    drops NUMBER(10),
     PRIMARY KEY (civilization_id, num_battle, type),
     FOREIGN KEY (civilization_id, num_battle) REFERENCES Battle_stats(civilization_id, num_battle)
 );
