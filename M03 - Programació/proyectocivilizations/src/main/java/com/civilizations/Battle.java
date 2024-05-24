f9ad439ab441f36cd09963e2b405701a1b66e803
Barg0s <dav.bargados@gmail.com>
2024-05-24 19:24:27 +0200

    ArreglosBattle
    cambios.

--- M03 - Programació/proyectocivilizations/src/main/java/com/civilizations/Battle.java
+++ M03 - Programació/proyectocivilizations/src/main/java/com/civilizations/Battle.java
@@ -1,24 +1,22 @@
 package com.civilizations;
 
 import java.util.ArrayList;
-
-
-
+import java.util.Random;
 
 public class Battle {
-    ArrayList<MilitaryUnit> civilizationArmy = new ArrayList<>();
-    ArrayList<MilitaryUnit> enemyArmy = new ArrayList<>();
-    //ArrayList<> armies = new ArrayList<>();
-    String battleDevelopment;
-    int initialCostFleet;    
-    int initialNumberUnitsCivilization;
-    int initialNumberUnitsEnemy;
-    int wasteWoodIron;
-    int enemyDrops;
-    int civilizationDrops;
-    int resourcesLooses ;
-    int initialArmies;
-    int actualNumberUnitsCivilization;
-    int actualNumberUnitsEnemy;
+    private ArrayList<MilitaryUnit> civilizationArmy = new ArrayList<>();
+    private ArrayList<MilitaryUnit> enemyArmy = new ArrayList<>();
+    private ArrayList<MilitaryUnit>[][] armies = new ArrayList[2][9];
+    private int[][] initialCostFleet;    
+    private int initialNumberUnitsCivilization; 
+    private int initialNumberUnitsEnemy;
+    private int[] wasteWoodIron = new int[2]; // [0]: madera, [1]: hierro
+    private int enemyDrops;
+    private int civilizationDrops;
+    private int[][] resourcesLooses = new int[2][4]; // [0]: civilización, [1]: enemigo
+    private int[][] initialArmies = new int[2][9];
+    private int[] actualNumberUnitsCivilization = new int[9];
+    private int[] actualNumberUnitsEnemy = new int[9];
+    private StringBuilder battleDevelopment = new StringBuilder();
 
-    };
+}
