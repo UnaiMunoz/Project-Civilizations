639e890cecff283a7d89830edd150f96f66495b2
Unai Muñoz <152631520+UnaiMunoz@users.noreply.github.com>
2024-05-29 18:53:20 +0200

    Update

--- M03 - Programació/proyectocivilizations/src/main/java/com/civilizations/AppData.java
+++ M03 - Programació/proyectocivilizations/src/main/java/com/civilizations/AppData.java
@@ -26,7 +26,7 @@ class AppData {
     private void connect() {
         String url = "jdbc:oracle:thin:@localhost:1521/xe";
         String user = "system";
-        String password = "david"; 
+        String password = "unai"; 
         try {
             // Load Oracle JDBC Driver
             Class.forName("oracle.jdbc.driver.OracleDriver");
