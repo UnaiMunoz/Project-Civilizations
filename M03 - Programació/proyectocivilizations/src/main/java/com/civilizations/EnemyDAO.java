e6cb8f1d83c444dbadc6c2f11f521d6a89207a4f
Barg0s <dav.bargados@gmail.com>
2024-06-05 13:05:20 +0200

    Incoming EnemyArmy
    metodo que hace un count de la tabla del ejercito enemigo.

--- M03 - Programació/proyectocivilizations/src/main/java/com/civilizations/EnemyDAO.java
+++ M03 - Programació/proyectocivilizations/src/main/java/com/civilizations/EnemyDAO.java
@@ -2,6 +2,7 @@ package com.civilizations;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class EnemyDAO {
@@ -23,5 +24,38 @@ public class EnemyDAO {
             System.out.println(e.getMessage());
         }
     }
-    
+    public int viewIncomingThreat(int id) {
+        String sql = "SELECT TYPE_UNIT, COUNT(*) AS count FROM ENEMY_THREAD WHERE CIVILIZATION_ID = ? GROUP BY TYPE_UNIT";
+        int swordsmanCount = 0;
+        int crossbowCount = 0;
+        int cannonCount = 0;
+        int spearmanCount = 0;    
+        try (Connection connection = AppData.getInstance().getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            try (ResultSet rs = statement.executeQuery()) {
+                while (rs.next()) {
+                    String typeUnit = rs.getString("TYPE_UNIT");
+                    int count = rs.getInt("count");
+                    switch (typeUnit) {
+                        case "Swordsman":
+                            swordsmanCount = count;
+                            break;
+                        case "Crossbow":
+                            crossbowCount = count;
+                            break;
+                        case "Cannon":
+                            cannonCount = count;
+                            break;
+                        case "Spearman":
+                            spearmanCount = count;
+                            break;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
